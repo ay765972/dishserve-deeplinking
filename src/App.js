fb1b606e1b81c67f71f89370b7cbee5df8f262e8
ankit <ankit.y@theluxurycloset.com>
2020-02-23 23:20:33 +0530

    seo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Helmet } from "react-helmet";
+import "./App.css";
 
 function App() {
+  const renderSeoTags = () => {
+    return (
+      <Helmet>
+        <title>dishServe</title>
+        <meta name="description" content="Taste is good"></meta>
+        <meta
+          property="og:title"
+          content="short title of your website/webpage"
+        />
+        <meta property="og:url" content="https://www.example.com/webpage/" />
+        <meta
+          property="og:description"
+          content="description of your website/webpage"
+        ></meta>
+        <meta property="og:image" content={logo}></meta>
+      </Helmet>
+    );
+  };
   return (
     <div className="App">
+      {renderSeoTags()}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
